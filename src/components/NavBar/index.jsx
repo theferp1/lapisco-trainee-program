e49980f5bcfc502a7cfc75d40ead002da9e63a4b
theferp1 <thiagoferportela@gmail.com>
2024-06-04 15:29:31 -0300

    Andando com relação a navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,18 +1,38 @@
-import { Container01, ImgBg, Container02, Bar, Circle } from "./style";
-import Bg from '../../../public/bgImage.png'
+import {
+  Container,
+  Container01,
+  NormalText,
+  Icon,
+  Container02,
+  TitleText,
+  Container03,
+  Bar,
+  Circle,
+} from "./style";
 
-function NavBar () {
-    return(
-        <Container01>
-            <ImgBg src={Bg}></ImgBg>
-            <Container02>
-                <Bar></Bar>
-                <Circle></Circle>
-                <Bar></Bar>
-            </Container02>
-        </Container01>
-    )
+import Bg from "../../../public/bgImage.png";
+import Menu from "../../../public/Menu.svg"
+
+function NavBar() {
+  return (
+    <Container>
+      <Container01>
+        <NormalText>Home</NormalText>
+        <NormalText>Trainee</NormalText>
+        <NormalText>Home</NormalText>
+        <Icon src={Menu}></Icon>
+      </Container01>
+      <Container02>
+        <TitleText>Lapisco</TitleText>
+        <TitleText>Trainee Program</TitleText>
+      </Container02>
+      <Container03>
+        <Bar />
+        <Circle />
+        <Bar />
+      </Container03>
+    </Container>
+  );
 }
-
 
 export default NavBar;
--- src/components/NavBar/style.jsx
+++ src/components/NavBar/style.jsx
@@ -1,34 +1,73 @@
 import styled from "styled-components";
 
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 80vh;
+
+  background-image: url('../../../public/bgImage.png');
+  border: solid 1px black;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  z-index: 0;  /* Added z-index for background image */
+`;
+
 export const Container01 = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+  align-items: center;
 
-    width: 100%;
-    height: 40%;
+  width: 100%;
+  height: 10%;
+  gap: 50px;
+  z-index: 1;
+`;
+
+
+export const NormalText = styled.div`
+    color: black;
 `
+export const Icon = styled.img`
 
-export const ImgBg = styled.img`
-    background-image: url(Bg);
-    background-image: url('sua-imagem.jpg'); /* Caminho para a sua imagem */
-    background-size: cover; /* Ajusta a imagem para cobrir toda a tela */
-    background-position: center; /* Centraliza a imagem */
-    background-attachment: fixed; /* Faz a imagem ficar fixa no fundo */
-    width: 100%;
-    height: auto;
 `
 export const Container02 = styled.div`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     justify-content: center;
+
+    width: 100%;
+    height: 40%;  
+   
+    padding-left: 10px;
+    fill-opacity: none;
+    border: solid 1px black;
+`
+export const TitleText = styled.div`
+    font-family: "Bebas Neue", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 70px;
+    
+    padding-left: 100px;
+
+    color:  #001B2E;
+`;
+
+export const Container03 = styled.div`
+    display: flex;
+    flex-direction: row;
+
     align-items: center;
 
     width: 100%;
-    height: 200px;
+    height: 50%;  
 
-
-    
+    border: solid 1px black;
 `
 
 export const Bar = styled.div`
@@ -40,7 +79,7 @@ export const Bar = styled.div`
 
 export const Circle = styled.div`
     width: 20%;
-    height: fit-content;
+    height: 245px;
 
     border-radius: 50%;
     background-color: #001B2E;
