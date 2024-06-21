a9f60682c5330516f4fbd2060cf999d492509aba
theferp1 <thiagoferportela@gmail.com>
2024-06-21 10:11:23 -0300

    formatando documento e melhorando nomeclatura de variaveis

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -7,63 +7,60 @@ import {
   TitleText,
   Container03,
   ContainerBar,
-  Bar,
-  ContainerCircle,
-  Circle,
-  ContainerEnd,
-  CircleTwo,
-  CircleDiv
+  Semicircle,
+  MiddleCircle,
 } from "./style";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 import Menu from "../../../public/Menu.svg";
 
 function NavBar() {
-
   /*Logica para o scroll da NavBar */
   const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 250) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 250) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-        window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <Container>
       <Container01 scrolled={scrolled}>
-          <NormalText scrolled={scrolled}><a href='#'>Home</a></NormalText>
-          <NormalText scrolled={scrolled}><a href='#'>Trainee</a></NormalText>
-          <NormalText scrolled={scrolled}><a href='#'>About us</a></NormalText>
+        <NormalText scrolled={scrolled}>
+          <a href="#">Home</a>
+        </NormalText>
+        <NormalText scrolled={scrolled}>
+          <a href="#">Trainee</a>
+        </NormalText>
+        <NormalText scrolled={scrolled}>
+          <a href="#">About us</a>
+        </NormalText>
         <Icon src={Menu} scrolled={scrolled}></Icon>
       </Container01>
       <Container02>
         <TitleText>Lapisco</TitleText>
         <TitleText>Trainee Program</TitleText>
       </Container02>
-      <Container03>  
-        <ContainerEnd>
-          <CircleTwo>
-            <CircleDiv>
-              <img src="lapiscoLogo.svg" width={'80%'} />
-            </CircleDiv>
-          </CircleTwo>
-        </ContainerEnd>
-  
+      <Container03>
+        <ContainerBar>
+          <Semicircle>
+            <MiddleCircle>
+              <img src="lapiscoLogo.svg" width={"80%"} />
+            </MiddleCircle>
+          </Semicircle>
+        </ContainerBar>
       </Container03>
-
-
-
     </Container>
   );
 }
--- src/components/NavBar/style.jsx
+++ src/components/NavBar/style.jsx
@@ -7,15 +7,14 @@ export const Container = styled.div`
   width: 100%;
   height: 60vh;
 
-  background-image: url('../../../public/bgImage.png');
-  
+  background-image: url("../../../public/bgImage.png");
+
   background-size: initial;
   background-position: center;
   position: relative;
 
   z-index: 0;
 `;
-
 
 export const Container01 = styled.nav`
   display: flex;
@@ -31,137 +30,116 @@ export const Container01 = styled.nav`
 
   position: fixed;
   transition: background-color 0.5s ease, border 0.5s ease;
+  border: solid 1px yellow;
 
-  background-color: ${({ scrolled }) => (scrolled ? ' #001B2E' : 'transparent')};
+  background-color: ${({ scrolled }) =>
+    scrolled ? " #001B2E" : "transparent"};
 `;
 
 export const NormalText = styled.p`
+  background-color: transparent;
+
+  & a {
     background-color: transparent;
+    color: ${({ scrolled }) => (scrolled ? "#ffffff" : "#e3e3e3")};
+    text-decoration: none;
 
-    & a {
-        background-color: transparent;
-        color: ${({ scrolled }) => (scrolled ? '#ffffff' : '#e3e3e3')};
-        text-decoration: none;
+    &:hover {
+      color: #b14141;
 
-        &:hover {
-            color: #b14141;
-
-            &::after {
-                transform: scaleX(1);
-                transform-origin: bottom left;
-            }
-        }
+      &::after {
+        transform: scaleX(1);
+        transform-origin: bottom left;
+      }
     }
-`;  
+  }
+`;
 
 export const Icon = styled.img`
-    background-color: transparent;
-    color: ${({ scrolled }) => (scrolled ? '#ffffff' : '#e3e3e3')};
+  background-color: transparent;
+  color: ${({ scrolled }) => (scrolled ? "#ffffff" : "#e3e3e3")};
 `;
 
 export const Container02 = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    background-color: transparent;
-    width: 100%;
-    height: 40%;  
-   
-    margin-top: 5%;
-    padding-left: 10px;
-    fill-opacity: none;
-`
-export const TitleText = styled.p`
-    font-family: "Bebas Neue", sans-serif;
-    font-weight: 400;
-    font-style: normal;
-    font-size: 70px;
-    background-color: transparent;
-    
-    padding-left: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: transparent;
+  width: 100%;
+  height: 40%;
 
-    color:  #e3e3e3;
+  margin-top: 5%;
+  padding-left: 10px;
+  fill-opacity: none;
+  border: solid 1px yellow;
+`;
+export const TitleText = styled.p`
+  font-family: "Bebas Neue", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 70px;
+  background-color: transparent;
+
+  padding-left: 100px;
+
+  color: #e3e3e3;
 `;
 
-
-export const ContainerBar = styled.div`
-    display: flex;
-    align-items: end;
-    justify-content: end;    
-    width: 100%;
-    border: solid 1px green;
-    background-color: transparent;
-    
-`
 export const Bar = styled.div`
-   
-    justify-content: center;
-    width: 100%;
-    height: 20px;
+  justify-content: center;
+  width: 100%;
+  height: 20px;
 
-    background-color: #C34036;
-    
-
-`
-export const ContainerCircle = styled.div`
-    width: 150px;
-    height: 150px;
-    background-color: blue;
-    border: solid 1px green;
-    margin-bottom: 50px;
-   
-`
-
-export const Circle = styled.div`
-  width: 150px; 
-  height: 150px;
-  margin-bottom: 200px;
-  background-color: #C34036;
-  border-radius: 50%; 
+  background-color: #c34036;
 `;
 
 export const Container03 = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: end;
+  display: flex;
+  flex-direction: row;
+  align-items: end;
 
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 
-    background-color: transparent;
+  background-color: transparent;
 `;
 
-export const ContainerEnd = styled.div`
-    position: relative;  /* Adicione esta linha */
-    background-color: #C34036;
-    height: 12%;
-    width: 100%;
+export const ContainerBar = styled.div`
+  position: relative;
+  background-color: #c34036;
+  height: 12%;
+  width: 100%;
+
+  border: solid 1px yellow;
 `;
 
-export const CircleTwo = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    bottom: 0%;  /* Posiciona o semicírculo no fundo do ContainerEnd */
-    left: 50%;  /* Centraliza horizontalmente no ContainerEnd */
-    transform: translate(-50%, 75%);
-    height: 65px;  /* Metade da altura original */
-    width: 130px;  /* Largura total */
-    border-radius: 0 0 65px 65px;  /* Cria um semicírculo virado 180 graus */
-    background-color: #C34036;
-    z-index: 1;  /* Certifique-se de que o semicírculo esteja por cima */
+export const Semicircle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  bottom: 0%; /* Posiciona o semicírculo no fundo do ContainerEnd */
+  left: 50%; /* Centraliza horizontalmente no ContainerEnd */
+  transform: translate(-50%, 75%);
+  height: 65px; /* Metade da altura original */
+  width: 130px; /* Largura total */
+  border-radius: 0 0 65px 65px; /* Cria um semicírculo virado 180 graus */
+  background-color: #c34036;
+  z-index: 1; /* Certifique-se de que o semicírculo esteja por cima */
+
+  border: solid 1px yellow;
 `;
 
-export const CircleDiv = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
+export const MiddleCircle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    border: 2px solid white;  /* Adiciona uma borda branca */
-    height: 110px;
-    width: 110px;
-    border-radius: 50%;
-    margin-bottom: 50%;
-    background-color: #000914;
+  border: 2px solid white; /* Adiciona uma borda branca */
+  height: 110px;
+  width: 110px;
+  border-radius: 50%;
+  margin-bottom: 50%;
+  background-color: #000914;
+  border: solid 1px yellow;
 `;
